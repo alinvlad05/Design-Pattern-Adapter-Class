6aee6eaacf8fa973f98e5a8bcf30ec966f1904c8
Alin Vlad <alinvlad05@gmail.com>
2020-12-21 17:45:39 +0200

    Initial commit

---
+++ CheckboxAdapter.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class CheckboxAdapter extends Checkbox {
+    public CheckboxAdapter(String n) {
+        super(n);
+    }
+
+    public boolean isSelected() {
+        return getState();
+    }
+}
